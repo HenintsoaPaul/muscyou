13d84bbfabf9762610e2ef1445971a83f40f9d4f
henintsoapaul <henintsoapaul@gmail.com>
2026-02-08 15:59:42 +0300

    feat: exo screens

---
+++ lib/features/exercises/exercises_provider.dart
@@ -0,0 +1,31 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../data/models/exercise.dart';
+import '../../data/repositories/local_repository.dart';
+
+class ExercisesNotifier extends StateNotifier<List<Exercise>> {
+  final LocalRepository _repository;
+
+  ExercisesNotifier(this._repository) : super([]) {
+    _loadExercises();
+  }
+
+  void _loadExercises() {
+    state = _repository.getExercises();
+  }
+
+  Future<void> addExercise(Exercise exercise) async {
+    await _repository.saveExercise(exercise);
+    state = [...state, exercise];
+  }
+
+  // Method to refresh if needed (e.g. after sync)
+  void refresh() {
+    _loadExercises();
+  }
+}
+
+final exercisesProvider =
+    StateNotifierProvider<ExercisesNotifier, List<Exercise>>((ref) {
+      final repository = ref.watch(localRepositoryProvider);
+      return ExercisesNotifier(repository);
+    });
